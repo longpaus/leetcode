4f3f55d5859463a4125d9cdb25dfe2f5fc1184dc
Hoang Long <longpaus@gmail.com>
2022-08-15 22:12:01 +1000

    did 1 but in java

---
+++ java/1.two-sum.java
@@ -0,0 +1,17 @@
+package java;
+
+import java.util.HashMap;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+         HashMap<Integer,Integer> map=new HashMap<Integer,Integer>();
+        for(int i = 0; i < nums.length; i++){
+            if(map.containsKey(target - nums[i])){
+                int[] result = {map.get(target - nums[i]),i};
+                return result;
+            }
+            map.put(nums[i],i);
+        }
+        return new int[0];
+    }
+}
