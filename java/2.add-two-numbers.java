92491c0b51c5dbfd4ec1cd86f9cb88baf4d6fe63
Hoang Long <longpaus@gmail.com>
2022-08-15 17:09:41 +1000

    made add two numbers faster

--- java/2.add-two-numbers.java
+++ java/2.add-two-numbers.java
@@ -17,54 +17,35 @@
 //  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode headSum = new ListNode(0);
-        ListNode curr = headSum;
-        int carry = 0;
-        int number = 0;
-        while (l1 != null || l2 != null) {
-            int l1Number = (l1 == null) ? 0 : l1.val;
-            int l2Number = (l2 == null) ? 0 : l2.val;
-            if ((l1 == null || l1.next == null) && (l2 == null || l2.next == null)) {
-                number = getNumber(l1Number + l2Number + carry);
-                curr.next = new ListNode(number);
-                curr = curr.next;
-                carry = getCarry((l1Number + l2Number + carry));
-                if (carry != 0) {
-                    curr.next = new ListNode(carry);
-
+        public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+            ListNode headSum = new ListNode(0);
+            ListNode curr = headSum;
+            int carry = 0;
+            int number = 0;
+            while(l1 != null || l2 != null){
+                int l1Number = (l1 == null) ? 0 : l1.val;
+                int l2Number = (l2 == null) ? 0 : l2.val;
+                if((l1 != null && l1.next != null) || (l2 != null && l2.next != null) ){
+                    number= (l1Number + l2Number + carry)%10;
+                    curr.next = new ListNode(number);
+                    curr = curr.next;
+                    carry = (l1Number + l2Number + carry)/10;
+                    
+                   
+                }else{
+                    number= (l1Number + l2Number + carry)%10;
+                    curr.next = new ListNode(number);
+                    curr = curr.next;
+                    carry = (l1Number + l2Number + carry)/10;
+                    if(carry != 0){
+                        curr.next = new ListNode(carry);                   
+                    }
                 }
-            } else {
-                number = getNumber(l1Number + l2Number + carry);
-                curr.next = new ListNode(number);
-                curr = curr.next;
-                carry = getCarry((l1Number + l2Number + carry));
+                l1 = (l1 != null) ? l1.next : null;
+                l2 = (l2 != null) ? l2.next : null;
             }
-
-            if (l1 != null) {
-                l1 = l1.next;
-            }
-            if (l2 != null) {
-                l2 = l2.next;
-            }
-
+          return headSum.next;
         }
-        return headSum.next;
-    }
-
-    public static int getCarry(int number) {
-        int carry = 0;
-        while (number >= 10) {
-            number -= 10;
-            carry++;
-        }
-        return carry;
-    }
-
-    public static int getNumber(int number) {
-        while (number >= 10) {
-            number -= 10;
-        }
-        return number;
     }
 }
 
