d164e5ede3f29ad6a3a7b67f4d368b54e19b33bb
Hoang Long <longpaus@gmail.com>
2022-09-06 00:11:29 +1000

    did 1282

---
+++ python/1282.groupThePeople.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+class Solution:
+    def groupThePeople(self, groupSizes: list[int]) -> list[list[int]]:
+        map = {}
+        for i in range(len(groupSizes)):
+            if groupSizes[i] not in map:
+                map[groupSizes[i]] = [[i]]
+            else:
+                if len(map[groupSizes[i]][-1]) < groupSizes[i]:
+                    map[groupSizes[i]][-1].append(i)
+                elif len(map[groupSizes[i]][-1]) >= groupSizes[i]:
+                    map[groupSizes[i]].append([i])
+        ans = []
+        for key in map:
+            for group in map[key]:
+                ans.append(group)
+        return ans
+                
+
+
+
+
